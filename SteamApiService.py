384bbb730eba745260e91b436c354227e0c52a17
Aytan <aytan@editsystems.com>
2023-03-17 01:01:14 +0300

    push local to glitch

---
+++ SteamApiService.py
@@ -0,0 +1,158 @@
+import json
+import requests
+import logging
+import os
+
+error_logs = []
+
+# Define constants for API URL and key
+API_URL = 'https://api.rawg.io/api/games'
+API_KEY = os.environ.get('SECRET_KEY')
+
+
+# Define default parameters for API request
+DEFAULT_PARAMS = {
+    'page_size': 40,
+    'page': 1,
+    'stores': 1,
+    'ordering': 'rating -released',
+    'ratings_count': 1,
+}
+
+
+
+def get_currency_price(region, appid):
+    url = f'https://store.steampowered.com/api/appdetails?appids={appid}&cc={region}'
+    response = requests.get(url)
+    data = json.loads(response.content)
+    return get_final_price(data[str(appid)])
+
+
+def get_final_price(data):
+    try:
+        return data['data']['price_overview']['final'] / 100
+    except KeyError:
+        error_logs.append(data['data'])
+        print("KeyError")
+        print(KeyError)
+        return 0
+
+
+def percentage_difference(europe_price: float, turkey_price: float) -> float:
+    if europe_price == 0 or turkey_price == 0:
+        print("Price is 0")
+        print("europe_price")
+        print(europe_price)
+        print("turkey_price")
+        print(turkey_price)
+        return 0
+    return (europe_price - turkey_price) / turkey_price * 100
+
+
+def get_over_price_amount(appid):
+    tl = get_currency_price("tr", appid)
+    usd = get_currency_price("us", appid) * 18.98
+    result = percentage_difference(usd, tl)
+    if result == 0:
+        error_logs.append(appid)
+
+    # print(f"The game is {result:.2f}% more expensive in USD.")
+    return result
+
+
+def get_games_from_tag(tags):
+    """Fetches a list of game names from RAWG API based on tags.
+
+    Args:
+        tags (str): A comma-separated string of tags to filter games by.
+
+    Returns:
+        list[str]: A list of game names matching the tags.
+    """
+    # Add tags and key to the default parameters
+    params = DEFAULT_PARAMS.copy()
+    params['tags'] = tags
+    params['key'] = API_KEY
+
+    # Initialize an empty list to store game names
+    game_names = []
+
+    # Make the first request with the given parameters
+    response = make_request(API_URL, params)
+
+    # Loop until there are no more pages to fetch
+    while response:
+        # Parse the JSON data from the response
+        data = parse_json(response)
+
+        # Extract the game names from the results and append them to the list
+        for result in data['results']:
+            game_names.append(result['name'])
+
+        # Check if there is a next page URL in the data
+        next_url = data.get('next')
+
+        if next_url:
+            # Make another request with the next page URL
+            response = make_request(next_url)
+        else:
+            # Break out of the loop if there is no next page URL
+            break
+
+    return game_names
+
+
+def make_request(url, params=None):
+    """Makes a GET request to a given URL with optional parameters.
+
+    Args:
+        url (str): The URL to make the request to.
+        params (dict): The query parameters for the request.
+
+    Returns:
+        requests.Response: The response object from the request.
+
+    Raises:
+        requests.exceptions.RequestException: If there is an error with the request.
+
+     """
+    try:
+        # Make a GET request with the given URL and parameters
+        response = requests.get(url, params=params)
+
+        # Check if the status code indicates success
+        response.raise_for_status()
+
+        return response
+    except requests.exceptions.RequestException as e:
+        # Log any error with the request
+        logging.error(f'Error making request: {e}')
+
+
+def parse_json(response):
+    """Parses JSON data from a given response object.
+
+    Args:
+        response (requests.Response): The response object containing JSON data.
+
+    Returns:
+        dict: The parsed JSON data as a dictionary.
+
+    Raises:
+        json.decoder.JSONDecodeError: If there is an error decoding JSON data.
+
+     """
+    try:
+        # Decode JSON data from text attribute of response object
+        data = response.json()
+
+        return data
+    except json.decoder.JSONDecodeError as e:
+        # Log any error decoding JSON data
+        logging.error(f'Error parsing JSON: {e}')
+
+# Call the function with some tags
+games = get_games_from_tag('souls-like')
+
+# Print the list of game names
+print(games)
--- server.py
+++ server.py
@@ -1,58 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#       _                              
-#      | |                             
-#    __| |_ __ ___  __ _ _ __ ___  ___ 
-#   / _` | '__/ _ \/ _` | '_ ` _ \/ __|
-#  | (_| | | |  __/ (_| | | | | | \__ \
-#   \__,_|_|  \___|\__,_|_| |_| |_|___/ .
-#
-# A 'Fog Creek'–inspired demo by Kenneth Reitz™
-
 import os
 from flask import Flask, request, render_template, jsonify
+import SteamApiService
 
 # Support for gomix's 'front-end' and 'back-end' UI.
 app = Flask(__name__, static_folder='public', template_folder='views')
 
 # Set the app secret key from the secret environment variables.
 app.secret = os.environ.get('SECRET')
-
-# Dream database. Store dreams in memory for now. 
-DREAMS = ['Python. Python, everywhere.']
-
-
-@app.after_request
-def apply_kr_hello(response):
-    """Adds some headers to all responses."""
-  
-    # Made by Kenneth Reitz. 
-    if 'MADE_BY' in os.environ:
-        response.headers["X-Was-Here"] = os.environ.get('MADE_BY')
-    
-    # Powered by Flask. 
-    response.headers["X-Powered-By"] = os.environ.get('POWERED_BY')
-    return response
+print("app.secret")
+print(app.secret)
+print("app")
+print(app)
 
 
 @app.route('/')
 def homepage():
     """Displays the homepage."""
     return render_template('index.html')
-    
-@app.route('/dreams', methods=['GET', 'POST'])
-def dreams():
-    """Simple API endpoint for dreams. 
-    In memory, ephemeral, like real dreams.
+
+
+@app.route('/', methods=['POST'])
+def submit():
+    """returns the price difference.
     """
-  
-    # Add a dream to the in-memory database, if given. 
-    if 'dream' in request.args:
-        DREAMS.append(request.args['dream'])
-    
-    # Return the list of remembered dreams. 
-    return jsonify(DREAMS)
+    data = SteamApiService.price_dif()
+    # Return the list of remembered dreams.
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run()
